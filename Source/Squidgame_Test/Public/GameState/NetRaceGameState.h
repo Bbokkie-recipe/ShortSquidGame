b4a06b9052bd47850d454690eebc9e8f53b52337
Bbokkie <rbbokkie@gmail.com>
2024-01-13 21:58:47 +0900

    [Add] Game State for Net Race

---
+++ Source/Squidgame_Test/Public/GameState/NetRaceGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "NetRaceGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUIDGAME_TEST_API ANetRaceGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
