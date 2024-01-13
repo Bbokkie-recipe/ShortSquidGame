15acf8e7e31f7c879dc93e3204daf0da924cf1dd
Bbokkie <rbbokkie@gmail.com>
2024-01-13 22:08:41 +0900

    [Add] GS, PS in NetRaceGameMode

---
+++ Source/Squidgame_Test/Public/PlayerState/NetRacePlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "NetRacePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUIDGAME_TEST_API ANetRacePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
