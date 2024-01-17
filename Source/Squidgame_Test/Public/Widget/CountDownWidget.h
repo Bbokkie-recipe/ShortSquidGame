b527f646d1198934985273af9f4159da67f5021b
Bbokkie <rbbokkie@gmail.com>
2024-01-18 00:17:55 +0900

    [Add] countDown widget

---
+++ Source/Squidgame_Test/Public/Widget/CountDownWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CountDownWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SQUIDGAME_TEST_API UCountDownWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
