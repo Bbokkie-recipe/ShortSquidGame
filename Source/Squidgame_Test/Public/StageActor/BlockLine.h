6548d70f9180181465e8997d7581667729d96e12
jonius12 <146903981+jonius12@users.noreply.github.com>
2024-01-19 20:13:02 +0900

    [Add] Start Line, FinLine :  Block function [Ignore] BlockLine Actor doesn't work

---
+++ Source/Squidgame_Test/Public/StageActor/BlockLine.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BlockLine.generated.h"
+
+UCLASS()
+class SQUIDGAME_TEST_API ABlockLine : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABlockLine();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
